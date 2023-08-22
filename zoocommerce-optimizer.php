002763b7c83bb85ef0aa215740b16830974cfa4d
Tom Broucke <tom@tombroucke.be>
2023-08-22 09:14:01 +0200

    chore(bump): Bump version to 1.1.0

--- zoocommerce-optimizer.php
+++ zoocommerce-optimizer.php
@@ -6,7 +6,7 @@ namespace Otomaties\ZooCommerceOptimizer;
  * Description:       Disable WooCommerce packages, admin features, admin notices, marketplace suggestions, extensions submenu, and more.
  * Author:            Tom Broucke
  * Author URI:        https://tombroucke.be
- * Version:           1.0.1
+ * Version:           1.1.0
  * License:           GPL2
  */
 
